524d0894784065efafd91e35f9450059b7bb8099
Sanju <sdhar3700@gmail.com>
2026-06-10 11:20:39 +0530

    [Sanju].add-Level_1 questions

---
+++ core-java/gcr-codebae/programming-elements/Level-1/Age_Calculator.java
@@ -0,0 +1,9 @@
+public class Age_Calculator {
+    public static void main(String[] args) {
+        int Birthyear=2002;
+        int CurrentYear=2025;
+        int Age=CurrentYear-Birthyear;
+        System.out.println("Now your age:" + Age);
+    }
+    
+}
